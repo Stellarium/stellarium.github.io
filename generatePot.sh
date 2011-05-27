4a645790bc178a2ac045f5e47b9dca4b24454075
Fabien Chéreau <fabien.chereau@gmail.com>
2011-05-28 01:11:19 +0200

    Cleaned up.

--- generatePot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xgettext -o stellarium-website.pot --keyword=q_ -C --copyright-holder="Stellarium's team" --from-code=utf-8 --package-name stellarium-website --package-version 1.0 -L PHP --msgid-bugs-address=stellarium-translation@lists.launchpad.net index.php
