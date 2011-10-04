28fc80dcd68e47d0e8d402784ed4b772b351d95d
Alexander Wolf <alex.v.wolf@gmail.com>
2011-10-04 15:20:26 +0700

    Update utils

--- util/compileMo.sh
+++ util/compileMo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for lang in fr de es
+for lang in ar be bs pt_BR bg ca hr cs nl fi fr de el hrx it ja pl pt ru es uk
 do
 #    touch po/$lang.po
 #    msgmerge -U po/$lang.po stellarium-website.pot
