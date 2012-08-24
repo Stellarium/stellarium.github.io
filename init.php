cd12f74303565c2cd4c5ed7d116fb8aa0dcfb127
Alexander Wolf <alex.v.wolf@gmail.com>
2012-08-24 19:34:04 +0700

    update linux binaries link

--- init.php
+++ init.php
@@ -51,12 +51,12 @@ if ($locale == "ar") {
     $langdir = "ltr";
 }
 
-$version = '0.11.4';
+$version = "0.11.4";
 $download_link_win32	= "http://sourceforge.net/projects/stellarium/files/Stellarium-win32/".$version."/stellarium-".$version."-win32.exe/download";
 $download_link_win64	= "http://sourceforge.net/projects/stellarium/files/Stellarium-win32/".$version."/stellarium-".$version."-win64.exe/download";
 $download_link_osx_u	= "http://sourceforge.net/projects/stellarium/files/Stellarium-MacOSX/".$version."/Stellarium-".$version.".dmg/download";
 $download_link_source	= "http://sourceforge.net/projects/stellarium/files/Stellarium-sources/".$version."/stellarium-".$version.".tar.gz/download";
-$download_link_linux	= "http://sourceforge.net/projects/stellarium/files/Stellarium-sources/".$version."/";
+$download_link_linux	= "http://sourceforge.net/projects/stellarium/files/Stellarium-linux/".$version."/";
 $download_link_ppa	= "https://launchpad.net/~stellarium/+archive/stellarium-releases";
 
 ?>
