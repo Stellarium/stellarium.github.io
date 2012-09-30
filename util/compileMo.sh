51aff8fdecc38cbeffc2402ed230db94c3632d16
Alexander Wolf <alex.v.wolf@gmail.com>
2012-09-30 21:30:18 +0700

    added Albanian, Esperanto, Georgian, Hungarian and Korean languages

--- util/compileMo.sh
+++ util/compileMo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for lang in ar be bs pt_BR bg ca zh zh_CN hr cs nb nl fi fr de el hrx id it ja pl pt ru sk sv es eu uk
+for lang in ar be bs pt_BR bg ca zh zh_CN hr cs nb nl fi fr de el hu hrx id it ja pl pt ru sk sv sq ka ko es eu eo uk
 do
 #    touch po/$lang.po
 #    msgmerge -U po/$lang.po stellarium-website.pot
