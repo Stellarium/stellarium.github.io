c10731e8eb020b0ff0434a15f5722dd8b5cb57d2
Alexander Wolf <alex.v.wolf@gmail.com>
2012-10-19 18:55:48 +0700

    revert download bar

--- screenshots.php
+++ screenshots.php
@@ -50,12 +50,50 @@ print '</select>  </div>';
 
 printf('
 <div id="header">
-	<div id="latestversion" class="block"><a href="/%s/">%s %s</a></div>',
-$locale,
+    <div id="latestversion" class="block"><a href="/">%s %s</a></div>
+	<div id="downloadbar" class="block">
+	    <div id="release" class="block">
+		<div class="download linux">
+		    <a href="%s">Linux<span>(%s)</span></a>
+		</div>
+		<div class="download linux">
+		    <a href="%s">Linux<span>(%s)</span></a>
+		</div>
+		<div class="download macosx">
+		    <a href="%s">Mac OS X<span>%s</span></a>
+		</div>
+		<div class="download windows">
+		    <a href="%s">Windows<span>%s</span></a>
+		</div>
+		<div class="download windows">
+		    <a href="%s">Windows<span>%s</span></a>
+		</div>
+		<div class="download ubuntu">
+		    <a href="%s">Ubuntu<span>%s</span></a>
+		</div>
+	    </div>
+	    <div id="additional" class="block">
+		<div class="download nopdf">
+		    <a href="http://www.stellarium.org/wiki/index.php/Stellarium_User_Guide">%s</a>
+		</div>
+	    </div>
+	</div>
+    </div>',
 q_('latest version'),
-$version);
-
-print displayDownloadBar()."</div>";
+$version,
+$download_link_source,
+q_('source'),
+$download_link_linux,
+q_('binaries'),
+$download_link_osx_u,
+$osx_version,
+$download_link_win32,
+q_('32 bit'),
+$download_link_win64,
+q_('64 bit'),
+$download_link_ppa,
+q_('latest stable release'),
+q_('User guide'));
 
 printf('  
 <div id="home">
