87a396c1b5fd01567aba495f5af3f08cf6554c41
Alexander Wolf <alex.v.wolf@gmail.com>
2013-09-08 17:19:24 +0700

    up to 0.12.3

--- init.php
+++ init.php
@@ -70,8 +70,8 @@ if (($locale == "ar") or ($locale == "fa")) {
     $langdir = "ltr";
 }
 
-$version	= "0.12.2";
-$next_version	= "0.12.3";
+$version	= "0.12.3";
+$next_version	= "0.13.0";
 $osx_version 	= "10.6+";
 $download_link_win32	= "http://sourceforge.net/projects/stellarium/files/Stellarium-win32/".$version."/stellarium-".$version."-win32.exe/download";
 $download_link_win64	= "http://sourceforge.net/projects/stellarium/files/Stellarium-win32/".$version."/stellarium-".$version."-win64.exe/download";
