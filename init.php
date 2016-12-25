2445a09c30e6926c788196233cfe9e89131dc039
Alexander Wolf <alex.v.wolf@gmail.com>
2016-12-25 17:18:43 +0700

    Updated version of stable Stellarium

--- init.php
+++ init.php
@@ -76,9 +76,9 @@ if (($locale == "ar") or ($locale == "fa")) {
     $langdir = "ltr";
 }
 
-$version	= "0.15.0";
+$version	= "0.15.1";
 $next_version	= "0.90.0";
-$guide_version	= "0.15.0-1";
+$guide_version	= "0.15.1-1";
 $osx_version 	= "10.8+";
 $OpenGL		= 0x00001000;
 $download_link_win32	= "http://sourceforge.net/projects/stellarium/files/Stellarium-win32/".$version."/stellarium-".$version."-win32.exe/download";
