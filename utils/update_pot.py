976e271c10b62c9b35d2eeb975111c61b554386d
Alexander Wolf <alex.v.wolf@gmail.com>
2018-06-01 19:55:16 +0700

    Updated script and template for translation

--- utils/update_pot.py
+++ utils/update_pot.py
@@ -13,7 +13,7 @@ src_dirs =  ['_includes/', '_layouts/', '_posts/', '']
 for d in src_dirs:
   all_files.extend(glob.glob(d + '*.html'))
 
-catalog = babel.messages.catalog.Catalog(domain=None, header_comment=u'# Translations template for PROJECT.\n# Copyright (C) YEAR ORGANIZATION\n# This file is distributed under the same license as the PROJECT project.\n# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.\n#', project="stellarium-website", version="1.0", copyright_holder="Stellarium's team", msgid_bugs_address="stellarium-translation@lists.launchpad.net", creation_date=None, revision_date=None, last_translator=None, language_team=None, charset=None, fuzzy=True)
+catalog = babel.messages.catalog.Catalog(domain=None, header_comment=u'# Translations template for PROJECT.\n# Copyright (C) YEAR ORGANIZATION\n# This file is distributed under the same license as the PROJECT project.\n# FIRST AUTHOR <EMAIL@ADDRESS>, YEAR.\n#', project="stellarium-website", version="1.0", copyright_holder="Stellarium's team", msgid_bugs_address="stellarium@googlegroups.com", creation_date=None, revision_date=None, last_translator=None, language_team=None, charset=None, fuzzy=True)
 for fn in all_files:
   with open(fn, 'r' ) as f:
     content = f.read()
