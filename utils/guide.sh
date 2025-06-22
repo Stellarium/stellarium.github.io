5eb9bf44ccdf69baaa5eb7c44c08cb95cd0a66cb
Alexander V. Wolf <alex.v.wolf@gmail.com>
2025-06-22 21:00:46 +0700

    Release v25.2

---
+++ utils/guide.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pdf2htmlEX --dest-dir ../guide --dpi 96 --embed-css 0 --embed-font 0 --embed-image 0 --embed-javascript 0 --embed-outline 0 ../files/guide.pdf index.html
--- utils/updateHeadDocs.sh
+++ utils/updateHeadDocs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-version="25.1"
+version="25.2"
 
 find ../doc/head/ -type f -name '*.html' -exec  sed -i "s/>$version</>HEAD</g" "{}" \;
